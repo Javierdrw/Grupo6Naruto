5b2fb877a57d9f3796a214a2e28c0ccd0092ad48
Javierdrw <javier.jg359@gmail.com>
2024-09-04 11:41:33 -0500

    afinando detalles

--- script/main.js
+++ script/main.js
@@ -1,7 +1,10 @@
 const api = "https://narutodb.xyz/api/"
 
-fetch(api+"character")
+fetch(api+"tailed-beast")
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        let arrayBestias = data.tailedBeasts
+        for (let i = 0; i < arrayBestias.length; i++) {
+       console.log(arrayBestias[i].images);}
+       
     })
